848dac13deb7403d583fece943d8caa5b8a001bb
Mykhailo Didur <mikhaildidur2003@gmail.com>
2022-03-12 15:56:19 +0200

    Design the project
    Add comments that describes project idea
    
    Signed-off-by: Mykhailo Didur <mikhaildidur2003@gmail.com>

---
+++ include/pap.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * Here should be created:
+ * 1. plugin_loader class
+ *      1. should be implemented with template to
+ *         load instance of template abstract class
+ *
+ * 2. plugin framework
+ *      2.1 should be implemented with macroses
+ *      2.2 the purpose is to create plugin available
+ *          to be exported as dynamic lib
+ *
+ */
